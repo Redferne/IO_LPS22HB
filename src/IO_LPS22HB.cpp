1d55dba5fc77e9a798ddfae2db5847f87d03c20f
Redferne <mr.redferne@gmail.com>
2018-07-18 21:46:51 +0200

    Add SPI support

--- src/IO_LPS22HB.cpp
+++ src/IO_LPS22HB.cpp
@@ -12,116 +12,181 @@
   Written by Adrien Chapelet for IoThings
  ***************************************************************************/
 
+#include <Arduino.h>
 #include <Wire.h>
-
+#include <SPI.h>
 #include "IO_LPS22HB.h"
 
 IO_LPS22HB::IO_LPS22HB()
 {
 }
-
-void IO_LPS22HB::begin(uint8_t address) {
+bool IO_LPS22HB::begin(uint8_t address, uint8_t mode, int8_t cs, SPIClass * spi) {
 	_address = address;
-	Wire.begin();
-	write(LPS22HB_RES_CONF, 0x0); // resolution: temp=32, pressure=128
-	write(LPS22HB_CTRL_REG1, 0x00); // one-shot mode
+	_cs = cs;
+	_spi = spi;
+	_mode = mode;
+
+	if (_cs == -1) {
+		Wire.begin(); // Bad pratice...
+		if (whoAmI() != LPS22HB_WHO_AM_I_VALUE)
+			return false;
+		write8(LPS22HB_RES_CONF, 0x0); // normal mode
+		write8(LPS22HB_CTRL_REG1, _mode);
+	} else {
+		pinMode(_cs, OUTPUT);
+    digitalWrite(_cs, HIGH);
+		if (read8(LPS22HB_WHO_AM_I) != LPS22HB_WHO_AM_I_VALUE)
+			return false;
+
+		uint8_t data;
+		data = read8(LPS22HB_RES_CONF);
+		data &= ~LPS22HB_LP_MODE;
+		write8(LPS22HB_RES_CONF, data);
+		data = read8(LPS22HB_CTRL_REG1);
+		data &= ~LPS22HB_BDU_EN;
+		data |= mode;
+		write8(LPS22HB_CTRL_REG1, data);
+		data = read8(LPS22HB_CTRL_REG2);
+		data = data | LPS22HB_I2C_DIS;
+		write8(LPS22HB_CTRL_REG2, data);
+	}
+	return true;
 }
 
-byte IO_LPS22HB::whoAmI() {
-	Wire.beginTransmission(_address);
-	Wire.write(LPS22HB_WHO_AM_I);
-	Wire.endTransmission();
-	Wire.requestFrom(_address, 1);
-	return Wire.read();
+uint8_t IO_LPS22HB::whoAmI() {
+	if (_cs == -1) {
+		Wire.beginTransmission(_address);
+		Wire.write(LPS22HB_WHO_AM_I);
+		Wire.endTransmission();
+		Wire.requestFrom(_address, 1);
+		return Wire.read();
+	} else {
+		return read8(LPS22HB_WHO_AM_I);
+	}
 }
 
 float IO_LPS22HB::readPressure() {
-	write(LPS22HB_CTRL_REG2, 0x1);
 
-	if (status(0x1) < 0)
-		return 1.23;
-	//delay(50);
-	uint8_t pressOutH = read(LPS22HB_PRES_OUT_H);
-	uint8_t pressOutL = read(LPS22HB_PRES_OUT_L);
-	uint8_t pressOutXL = read(LPS22HB_PRES_OUT_XL);
+	if (_mode == LPS22HB_ODR_ONESHOT) {
+		uint8_t data = read8(LPS22HB_CTRL_REG2);
+		data |= 1;
+		write8(LPS22HB_CTRL_REG2, data); // Trigger oneshot measurement
+	}
+
+	if (status(LPS22HB_PDA))
+		return NAN;
+
+	uint8_t pressOutXL = read8(LPS22HB_PRES_OUT_XL);
+	uint8_t pressOutL = read8(LPS22HB_PRES_OUT_L);
+	uint8_t pressOutH = read8(LPS22HB_PRES_OUT_H);
 
 	long val = ( ((long)pressOutH << 24) | ((long)pressOutL << 16) | ((long)pressOutXL << 8)) >> 8;
-	//if (val == 1.00) readPressure();
-	return val/4096.0f;
+	return val / 4096.0f; // hPa
 }
 
 uint32_t IO_LPS22HB::readPressureRAW() {
-	write(LPS22HB_CTRL_REG2, 0x1);
 
-	if (status(0x1) < 0)
-		return 123;
-	//delay(50);
-	uint8_t pressOutH = read(LPS22HB_PRES_OUT_H);
-	uint8_t pressOutL = read(LPS22HB_PRES_OUT_L);
-	uint8_t pressOutXL = read(LPS22HB_PRES_OUT_XL);
+	if (_mode == LPS22HB_ODR_ONESHOT) {
+		uint8_t data = read8(LPS22HB_CTRL_REG2);
+		data |= 1;
+		write8(LPS22HB_CTRL_REG2, data); // Trigger oneshot measurement
+	}
+
+	if (status(LPS22HB_PDA))
+		return 0;
+
+	uint8_t pressOutXL = read8(LPS22HB_PRES_OUT_XL);
+	uint8_t pressOutL = read8(LPS22HB_PRES_OUT_L);
+	uint8_t pressOutH = read8(LPS22HB_PRES_OUT_H);
 
 	int32_t val = ((pressOutH << 24) | (pressOutL << 16) | (pressOutXL << 8));
 	val >> 8;
-	val=val+0x400000;
+	val = val + 0x400000;
 	//if (val == 1.00) readPressure();
 	return (uint32_t)val;
 }
 
 uint32_t IO_LPS22HB::readPressureUI() {
-	write(LPS22HB_CTRL_REG2, 0x1);
 
-	if (status(0x1) < 0)
-		return 1.23;
-	//delay(50);
-	uint8_t pressOutH = read(LPS22HB_PRES_OUT_H);
-	uint8_t pressOutL = read(LPS22HB_PRES_OUT_L);
-	uint8_t pressOutXL = read(LPS22HB_PRES_OUT_XL);
+	if (_mode == LPS22HB_ODR_ONESHOT) {
+		uint8_t data = read8(LPS22HB_CTRL_REG2);
+		data |= 1;
+		write8(LPS22HB_CTRL_REG2, data); // Trigger oneshot measurement
+	}
+
+	if (status(LPS22HB_PDA))
+		return 0;
+
+	uint8_t pressOutXL = read8(LPS22HB_PRES_OUT_XL);
+	uint8_t pressOutL = read8(LPS22HB_PRES_OUT_L);
+	uint8_t pressOutH = read8(LPS22HB_PRES_OUT_H);
 
 	uint32_t val = ((pressOutH << 24) | (pressOutL << 16) | (pressOutXL << 8)) >> 8;
-	//if (val == 1.00) readPressure();
 	return val/4096;
 }
 
 float IO_LPS22HB::readTemperature() {
-	write(LPS22HB_CTRL_REG2, 0x1);
-	if (status(0x2) < 0)
-		return 4.56;
 
-	uint8_t tempOutH = read(LPS22HB_TEMP_OUT_H);
-	uint8_t tempOutL = read(LPS22HB_TEMP_OUT_L);
+	if (_mode == LPS22HB_ODR_ONESHOT) {
+		uint8_t data = read8(LPS22HB_CTRL_REG2);
+		data |= 1;
+		write8(LPS22HB_CTRL_REG2, data); // Trigger oneshot measurement
+	}
+
+	if (status(LPS22HB_TDA))
+		return NAN;
+
+	uint8_t tempOutL = read8(LPS22HB_TEMP_OUT_L);
+	uint8_t tempOutH = read8(LPS22HB_TEMP_OUT_H);
 
 	int16_t val = (tempOutH << 8) | (tempOutL & 0xff);
 	return ((float)val)/100.0f;
 }
 
+bool IO_LPS22HB::status(uint8_t status) {
 
-uint8_t IO_LPS22HB::status(uint8_t status) {
-	int count = 1000;
-	uint8_t data = 0xff;
-	do {
-		data = read(LPS22HB_STATUS_REG);
-		--count;
-		if (count < 0)
-			break;
-	} while ((data & status) == 0);
-
-	if (count < 0)
-		return -1;
-	else
-		return 0;
+	uint8_t count = 1000 / 5;
+	uint8_t data;
+	while (count--) {
+		if (read8(LPS22HB_STATUS_REG) & status)
+			return false;
+		delay(5);
+	}
+	return true;
 }
 
-uint8_t IO_LPS22HB::read(uint8_t reg) {
-	Wire.beginTransmission(_address);
-	Wire.write(reg);
-	Wire.endTransmission();
-	Wire.requestFrom(_address, 1);
-	return Wire.read();
+uint8_t IO_LPS22HB::read8(uint8_t reg) {
+	uint8_t value;
+	if (_cs == -1) {
+		Wire.beginTransmission(_address);
+		Wire.write(reg);
+		Wire.endTransmission();
+		Wire.requestFrom(_address, 1);
+		return Wire.read();
+	} else {
+		_spi->beginTransaction(SPISettings(LPS22HB_SPI_SPEED, LPS22HB_SPI_BITORDER, LPS22HB_SPI_MODE));
+    digitalWrite(_cs, LOW);
+		_spi->transfer(reg | 0x80); // Read!
+		value = _spi->transfer(0);
+    digitalWrite(_cs, HIGH);
+		_spi->endTransaction();
+		return value;
+	}
 }
 
-void IO_LPS22HB::write(uint8_t reg, uint8_t data) {
-	Wire.beginTransmission(_address);
-	Wire.write(reg);
-	Wire.write(data);
-	Wire.endTransmission();
+void IO_LPS22HB::write8(uint8_t reg, uint8_t data) {
+	if (_cs == -1) {
+		Wire.beginTransmission(_address);
+		Wire.write(reg);
+		Wire.write(data);
+		Wire.endTransmission();
+	} else {
+		_spi->beginTransaction(SPISettings(LPS22HB_SPI_SPEED, LPS22HB_SPI_BITORDER, LPS22HB_SPI_MODE));
+    digitalWrite(_cs, LOW);
+		_spi->transfer(reg);
+		_spi->transfer(data);
+    digitalWrite(_cs, HIGH);
+		_spi->endTransaction();
+	}
+	return;
 }
--- src/IO_LPS22HB.h
+++ src/IO_LPS22HB.h
@@ -17,26 +17,45 @@
 #define _IO_LPS22HB_h
 #include <Arduino.h>
 
-#define LPS22HB_WHO_AM_I	0X0F //Who am I
-#define LPS22HB_RES_CONF	0X1A //Resolution
-#define LPS22HB_CTRL_REG1	0X10
-#define LPS22HB_CTRL_REG2	0X11
-#define LPS22HB_STATUS_REG	0X27
-#define LPS22HB_PRES_OUT_XL	0X28 //LSB
-#define LPS22HB_PRES_OUT_L	0X29
-#define LPS22HB_PRES_OUT_H	0X2A //MSB
-#define LPS22HB_TEMP_OUT_L	0X2B //LSB
-#define LPS22HB_TEMP_OUT_H	0X2C //MSB
+
+#define LPS22HB_WHO_AM_I	  0x0F //Who am I
+#define LPS22HB_RES_CONF	  0x1A //Resolution
+#define LPS22HB_CTRL_REG1	  0x10
+#define LPS22HB_CTRL_REG2	  0x11
+#define LPS22HB_STATUS_REG	0x27
+#define LPS22HB_PRES_OUT_XL	0x28 //LSB
+#define LPS22HB_PRES_OUT_L	0x29
+#define LPS22HB_PRES_OUT_H	0x2A //MSB
+#define LPS22HB_TEMP_OUT_L	0x2B //LSB
+#define LPS22HB_TEMP_OUT_H	0x2C //MSB
+
+#define LPS22HB_ODR_75_HZ   0x50
+#define LPS22HB_ODR_50_HZ   0x40
+#define LPS22HB_ODR_25_HZ   0x30
+#define LPS22HB_ODR_10_HZ   0x20
+#define LPS22HB_ODR_1_HZ    0x10
+#define LPS22HB_ODR_ONESHOT 0x0
+
+#define LPS22HB_TDA         0x02
+#define LPS22HB_PDA         0x01
+#define LPS22HB_LP_MODE     0x01
+#define LPS22HB_BDU_EN      0x02
+#define LPS22HB_ADD_INC     0x10
+#define LPS22HB_I2C_DIS     0x08
+#define LPS22HB_SW_RESET    0x04
+
 
 #define LPS22HB_WHO_AM_I_VALUE	0xB1 // Expected return value of WHO_AM_I register
 
-
+#define LPS22HB_SPI_SPEED           10000000
+#define LPS22HB_SPI_BITORDER        MSBFIRST
+#define LPS22HB_SPI_MODE            SPI_MODE0
 
 class IO_LPS22HB {
 public:
 	IO_LPS22HB();
 
-	void begin(uint8_t address);
+	bool begin(uint8_t address, uint8_t mode=LPS22HB_ODR_ONESHOT, int8_t cs=-1, SPIClass * spi=NULL);
 
 	uint8_t whoAmI();
 	float readTemperature();
@@ -47,9 +66,12 @@ public:
 
 private:
 	uint8_t _address;
-	uint8_t read(uint8_t reg);
-	void write(uint8_t reg, uint8_t data);
-	uint8_t status(uint8_t data);
+	uint8_t read8(uint8_t reg);
+	void write8(uint8_t reg, uint8_t data);
+	bool status(uint8_t data);
+	SPIClass * _spi;
+	int8_t _cs;
+	uint8_t _mode;
 };
 
 #endif
